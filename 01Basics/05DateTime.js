3665fd842cb2639ae74d7f47afb1a3f4ee1c2933
samiksha <samiksha.10708856@ltimindtree.com>
2024-11-21 10:47:32 +0530

    done with basics

---
+++ 01Basics/05DateTime.js
@@ -0,0 +1,25 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let MyOwnDate = new Date(2024, 0, 23)
+
+// console.log(MyOwnDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(MyOwnDate.getTime());
+// console.log(Math.floor(Date.now() / 1000));
+
+let newDate = new Date()
+// console.log(newDate);
+console.log(newDate.getMonth() + 1);
+
+
---
+++ 02Basics/01Arrays.js
@@ -0,0 +1,38 @@
+// const myArr = [0, 1, 2, 3, 4, 5, 6]
+
+// const newArr = myArr.join()
+
+// const myArr2 = new Array(1, 2, 3, 4, 5)
+// console.log(myArr[0]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr);
+// console.log(newArr);
+
+// slice, splice
+
+// const myn1 = myArr.slice(1, 3)
+// console.log(myn1);
+
+// const myn2 = myArr.splice(1, 3);
+// console.log(myn2);
+
+const marvel_heros = ["thor", "Ironman","Spiderman"]
+const Dc_Heros = ["superman","flash","batman"]
+
+// marvel_heros.push(Dc_Heros);
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(Dc_Heros)
+// console.log(allHeros);/
+
+
+
+
